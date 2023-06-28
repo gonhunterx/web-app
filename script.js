f432df3f964ebc98f3b4f3c7827c43a869db5da4
Jadon Lama <Jadonlama357@gmail.com>
2023-06-28 09:13:21 -0400

    Updated Features to the website
    Website to work on dev skills.

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.add('active');
+    }
+);
